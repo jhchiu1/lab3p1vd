a52a7d1a826d7102106e8f08dfa1adbbdf279d75
juliechiu <jhchiu1@gmail.com>
2018-01-23 17:17:41 -0600

    first commit

---
+++ db.py
@@ -0,0 +1,79 @@
+import sqlite3
+
+# Create db, cursor, and table
+try:
+    db = sqlite3.connect('db.add_juggler')
+    cursor = db.cursor()
+    cursor.execute('create table if not exists jugglers (name text, country text, catches int)')
+except sqlite3.Error:
+    print('Error creating table')
+finally:
+    db.close()
+
+# Add sample info to table
+try:
+    db = sqlite3.connect('jugglers.db')
+    cursor = db.cursor()
+    with db:
+        cursor.execute('insert into jugglers values ("Ian Stewart", "Canada", 94)')
+        cursor.execute('insert into jugglers values ("Aaron Gregg", "Canada", 88)')
+        cursor.execute('insert into jugglers values ("Chad Taylor", "USA", 78)')
+except sqlite3.Error as e:
+    print('Error adding rows:')
+    print(e)
+finally:
+    db.close()
+
+
+# Gets name, country, and catches from user, adds data to db
+def add_juggler(name, country, catches):
+    try:
+        db = sqlite3.connect('jugglers.db')
+        cursor = db.cursor()
+        with db:
+            cursor.execute('insert into jugglers values (?, ?, ?)', (name, country, catches))
+    except sqlite3.Error as e:
+        print('Error adding data to table:')
+        print(e)
+    finally:
+        db.close()
+
+
+# Gets juggler's name from the user, removes them from db
+def remove_juggler(name):
+    try:
+        db = sqlite3.connect('jugglers.db')
+        cursor = db.cursor()
+        with db:
+            cursor.execute('delete from jugglers where name=?', (name,))
+    except sqlite3.Error as e:
+        print('Error removing data from table:')
+        print(e)
+
+
+# Prints contents of jugglers table
+def show_all():
+    try:
+        db = sqlite3.connect('jugglers.db')
+        cursor = db.cursor()
+        for row in cursor.execute('select * from jugglers'):
+            print(row)
+    except sqlite3.Error as e:
+        print('Error printing data:')
+        print(e)
+    finally:
+        db.close()
+
+
+# Edits a juggler's number of catches
+def edit(name, catches):
+    try:
+        db = sqlite3.connect('jugglers.db')
+        cursor = db.cursor()
+        with db:
+            cursor.execute('update jugglers set catches=? where name=?', (catches, name))
+    except sqlite3.Error as e:
+        print('Error updating data:')
+        print(e)
+    finally:
+        db.close()
---
+++ ui1.py
@@ -0,0 +1,34 @@
+import db
+
+# Display menu options, validation added
+while True:
+    choice = input("1. Add \n2. Remove \n3. View all \n4. Edit\n5. Quit\n")
+
+    if choice != "1" and choice != "2" and choice != "3" and choice != "4" and choice != "5":
+        print("Please try again, enter a number between 1 and 5..\n")
+
+    # Ask user for juggler info to add
+    elif choice == "1":
+        name = input("Name?\n")
+        country = input("Country?\n")
+        catches = input("Catches?\n")
+        db.add_juggler(name, country, catches)
+
+    # Ask user for juggler info to remove
+    elif choice == "2":
+        name = input("Name?\n")
+        db.remove_juggler(name)
+
+    # Show all jugglers in db
+    elif choice == "3":
+        db.view_all()
+
+    # Ask user to update high score for juggler named
+    elif choice == "4":
+        name = input("Name?\n")
+        catches = input("New high score?\n")
+        db.edit(name, catches)
+
+    # Exit
+    elif choice == "5":
+        break
